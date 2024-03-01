c76815d871a9f0583fd6856a745254bc30e123f7
ShinHB417 <heebum417@naver.com>
2024-03-01 18:03:31 +0900

    feat : server deploy

--- src/main/java/docker/test/TestController.java
+++ src/main/java/docker/test/TestController.java
@@ -1,12 +1,39 @@
 package docker.test;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class TestController {
+
+    @Value("${server.env}")
+    private String env;
+    @Value("${server.port}")
+    private String serverPort;
+    @Value("${server.serverAddress}")
+    private String serverAddress;
     @GetMapping("/test")
     public String test(){
         return "hello docker cicd!!";
     }
+    @GetMapping("/hc")
+    public ResponseEntity<?> healthCheck(){
+        Map<String, String> responseData = new HashMap<>();
+        responseData.put("env", env);
+        responseData.put("serverPort", serverPort);
+        responseData.put("serverAddress", serverAddress);
+        return ResponseEntity.ok(responseData);
+    }
+    @GetMapping("/env")
+    public ResponseEntity<?> getEnv(){
+        Map<String, String> responseData = new HashMap<>();
+        return ResponseEntity.ok(env);
+    }
 }
+
+
